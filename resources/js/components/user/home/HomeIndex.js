5f0f624e4f5a5f2235514b1faa2eb0e928138a9d
m.hayakawa <m.hayakawa.ba@gmail.com>
2021-12-28 00:55:02 +0900

    reactで画面が映るようにした

---
+++ resources/js/components/user/home/HomeIndex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Helmet} from 'react-helmet';
+
+function HomeIndex() {
+  return (
+    <>
+      test
+    </>
+  );
+}
+
+export default HomeIndex;
